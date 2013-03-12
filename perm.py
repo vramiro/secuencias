44b77e331864ef8aab6a020433de9426de4ce55f
Victor Ramiro <vramiro@gmail.com>
2013-03-12 14:21:32 +0100

    sintetic versions of new permutations

--- perm.py
+++ perm.py
@@ -3,16 +3,9 @@ def perm(s,n):
     if n == 1:
         return tag(s)
     else:
-        ll = []
-        for s in tag(s):
-            ll = ll + perm(s, n-1)
-        return ll
+        return reduce(lambda x,y: x+y, [perm(s,n-1) for s in tag(s)])
 
 def tag(s):
-    # l = []
-    # for i in range(len(s)+1):
-    #     l.append(s[0:i] + '-' + s[i:])
-    # return l
     return [s[0:i] + '-' + s[i:] for i in range(len(s)+1)]
 
 ss=set(perm('ABC',4))
